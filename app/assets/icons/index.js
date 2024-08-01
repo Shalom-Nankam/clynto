206461e216330d95ff97a3d1b23a608da4fa05d9
code-digga <168196193+code-digga@users.noreply.github.com>
2024-08-01 09:25:47 +0100

    new fix

--- app/assets/icons/index.js
+++ app/assets/icons/index.js
@@ -1,19 +1,35 @@
-import Coin from './coins.png';
-import Plus from './plus.png';
-import Minus from './minus.png';
-import Arrowright from './arrowright.svg';
-import metamask from './metamask.svg';
-import bitcoin from './bitcoin.svg';
-import n from './n.svg';
-import funnel from './funnel.svg';
-import tabler from './tabler.svg';
-import hugeicons from './hugeicons.svg';
-import completed from './completed.svg';
-import inprogress from './inprogress.svg';
-import money from './money.svg';
-import warning from './warning.svg';
-import liquidation from './liquidation.svg';
+import Coin from "./coins.png";
+import Plus from "./plus.png";
+import Minus from "./minus.png";
+import Arrowright from "./arrowright.svg";
+import metamask from "./metamask.svg";
+import bitcoin from "./bitcoin.svg";
+import n from "./N.svg";
+import funnel from "./funnel.svg";
+import tabler from "./tabler.svg";
+import hugeicons from "./hugeicons.svg";
+import completed from "./completed.svg";
+import inprogress from "./inprogress.svg";
+import money from "./money.svg";
+import warning from "./warning.svg";
+import liquidation from "./liquidation.svg";
 
-const icons = {warning, liquidation, money,Coin,Plus,Minus,Arrowright, inprogress, completed,funnel, tabler, hugeicons, metamask,bitcoin,n};
+const icons = {
+  warning,
+  liquidation,
+  money,
+  Coin,
+  Plus,
+  Minus,
+  Arrowright,
+  inprogress,
+  completed,
+  funnel,
+  tabler,
+  hugeicons,
+  metamask,
+  bitcoin,
+  n,
+};
 
-export default icons 
+export default icons;
